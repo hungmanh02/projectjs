0880b12497ad3212757462ff19a079d61eb11178
hungmanh02 <domanh462@gmail.com>
2023-10-24 15:34:11 +0700

    add html css js tren github đovietduc ve

--- js-coban.js
+++ js-coban.js
@@ -90,3 +90,45 @@ if (true) {
   x = "scope in if";
 }
 console.log(x);
+
+// I Phân biệt cách khai báo biến với từ khóa var(trước khi có es6 ) với let, const (es6)
+// 1. Khai báo lại cùng tên biến trong cùng 1 phạm vi
+
+// var x = "global scope";
+// var x = "global scope 2";
+
+let x = "global scope 2";
+// let x = "global scope 2";
+// const và let không được khai báo biến trùng nhau khi đang ở global scope
+console.log(x);
+
+// 2. Scope : var -- function scope --- let --- block scope
+// var y = "global scope";
+// if (true) {
+//   var y = " global scope in if";
+// }
+// console.log(y);
+let y = "global scope";
+// if (true) {
+//   let y = " global scope in if";
+//   // let y trong và let y ngoài là 2 biên scope khác nhau, let y trong là scope của trong if còn, let y ngoài là global scope
+// }
+// sự dụng function
+// function changValue() {
+//   var y = "function scope";
+//   console.log(y);
+// }
+// changValue();
+// console.log(y);
+
+// 3. Hoisting -- var có -- let ,  const không
+// console.log(z);
+// let z = 20;
+// z = 30;
+// 4. Gán lại value hay còn gọi là update value -- var, let có, const không
+
+const z2 = {
+  name: "ducdo",
+};
+z2.name = "dung";
+console.log(z2);
