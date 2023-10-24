95253bde237bfbbf200db3bda8d6d33459574f16
hungmanh02 <domanh462@gmail.com>
2023-10-24 19:55:32 +0700

    kiểm tra field trong for

--- assets/js/pages/signup.js
+++ assets/js/pages/signup.js
@@ -10,63 +10,29 @@ const inputAllSelector = document.querySelectorAll(".form-group input");
 
 function handleSignUpClick(e) {
   e.preventDefault();
-  // 1. Thực hiện validate
-
-  // 1.1 validate input name
-  // let inputValueName = inputNameSelector.value;
-  // let errorNameSelector = inputNameSelector
-  //   .closest(".form-group")
-  //   .querySelector(".error_message"); // tìm tới class cha gần nhất rồi querySelector tới error_message
-  // if (inputValueName === "") {
-  //   inputNameSelector.classList.add("error"); // add class error để báo lỗi khi form trống , class error đã css
-  //   errorNameSelector.textContent = "Tên không được để trống";
-  // } else {
-  //   inputNameSelector.classList.remove("error"); // remove class error để báo lỗi khi form trống , class error đã css
-  //   errorNameSelector.textContent = "";
-  // }
-
-  // // 1.2 validate input email
-  // let inputValueEmail = inputEmailSelector.value;
-  // let errorEmailSelector = inputEmailSelector
-  //   .closest(".form-group")
-  //   .querySelector(".error_message");
-
-  // if (inputValueEmail === "") {
-  //   inputNameSelector.classList.add("error");
-  //   errorEmailSelector.textContent = "Email không được để trống";
-  // } else {
-  //   inputNameSelector.classList.remove("error");
-  //   errorEmailSelector.textContent = "";
-  // }
-
-  // // 1.3 validate input password
-  // let inputValuePassword = inputPasswordSelector.value;
-  // let errorPasswordSelector = inputPasswordSelector
-  //   .closest(".form-group")
-  //   .querySelector(".error_message");
-  // if (inputValuePassword === "") {
-  //   inputPasswordSelector.classList.add("error");
-  //   errorPasswordSelector.textContent = "Password không được để trống";
-  // } else {
-  //   inputPasswordSelector.classList.remove("error");
-  //   errorPasswordSelector.textContent = "";
-  // }
 
   // tối ưu validate
   // 1. Thực hiện validate
   for (let i = 0; inputAllSelector.length; i++) {
     let inputSelector = inputAllSelector[i];
+    // console.log(inputSelector);
     let valueInput = inputSelector.value;
     let divMessageSelector = inputSelector
       .closest(".form-group")
       .querySelector(".error_message");
+    let name = inputSelector.name;
     // validate  not empty
     if (valueInput === "") {
       // thêm viền đỏ cho input
       inputSelector.classList.add("error");
       // hiển thị message lỗi
-      let name = inputSelector.name;
       let message = name + " không được để trống";
+      divMessageSelector.textContent = message;
+    } else if (
+      valueInput.length < 3 &&
+      inputSelector.getAttribute("minValidate") === "have_validate"
+    ) {
+      let message = name + " tối thiểu 3 kí tự";
       divMessageSelector.textContent = message;
     }
   }
