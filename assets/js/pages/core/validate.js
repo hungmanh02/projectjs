4803d9ebefbad627fbd54d70948475624cf45c80
hungmanh02 <domanh462@gmail.com>
2023-11-02 15:15:11 +0700

    Hien thi loi ra form

--- assets/js/pages/core/validate.js
+++ assets/js/pages/core/validate.js
@@ -1,3 +1,67 @@
 function Validate(options) {
-  console.log("options:", options);
+  const regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  // 1. Lấy ra container bao đống form
+  const container = document.querySelector(options.container);
+  // 2. Tất cả các elements khác query dựa vào container
+  const btnSignUpSelector = container.querySelector(".btn-signup");
+  const rules = options.rules;
+  const messages = options.messages;
+  let errors;
+
+  const ruleMethod = {
+    required: function (valueInput, valueRule) {
+      return valueInput !== "";
+    },
+    minlength: function (valueInput, valueRule) {
+      return valueInput.length >= valueRule;
+    },
+    email: function (valueInput, valueRule) {
+      return regexEmail.test(valueInput);
+    },
+    equal_to: function (valueInput, valueRule) {
+      let passSelector = container.querySelector(`.${valueRule}`);
+      let valuePass = passSelector.value;
+      return valuePass === valueInput;
+    },
+  };
+
+  function initEventAndData() {
+    btnSignUpSelector.addEventListener("click", handleSignUpClick);
+  }
+  function handleSignUpClick(e) {
+    e.preventDefault();
+    errors = [];
+    for (const keyInputName in rules) {
+      const inputSelector = container.querySelector(`.${keyInputName}`);
+      const valueInput = inputSelector.value;
+      const ruleAllForInput = rules[keyInputName];
+      for (const ruleItemKey in ruleAllForInput) {
+        const valueRule = ruleAllForInput[ruleItemKey];
+        const result = ruleMethod[ruleItemKey](valueInput, valueRule);
+        const keyMessage = keyInputName + "_" + ruleItemKey;
+        if (!result) {
+          // đẩy lỗi vào biến đang lưu trữ
+          errors.push({
+            elementError: inputSelector,
+            message: messages[keyMessage]
+              ? messages[keyMessage]
+              : keyInputName + "not valid",
+          });
+          break;
+        }
+      }
+    }
+
+    // Hiện thị lỗi
+    console.log(errors);
+    errors.forEach(function (element) {
+      let inputElement = element.elementError;
+      let divError = inputElement.nextElementSibling;
+      inputElement.classList.add("error");
+      divError.textContent = element.message;
+    });
+  }
+
+  // add event listener + data init
+  initEventAndData();
 }
--- assets/js/pages/signup.js
+++ assets/js/pages/signup.js
@@ -1,3 +1,14 @@
 let signupInstanceValidate = new Validate({
-  container: "#page_regsiter",
+  container: ".form_register",
+  rules: {
+    name: { required: true },
+    email: { required: true, minlength: 3, email: true },
+    password: { required: true, minlength: 8 },
+    confirm_password: { required: true, minlength: 8, equal_to: "password" },
+  },
+  messages: {
+    name_required: "Tên không được để trống",
+    email_required: "Email không được để trống",
+    password_required: "Mật khẩu không được để trống",
+  },
 });
