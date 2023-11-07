08f48ff3a469abb858348a388b687a74793ff476
hungmanh02 <domanh462@gmail.com>
2023-11-07 12:01:36 +0700

    84. thuc hien validate form login bang thu vien validate

--- assets/js/pages/login.js
+++ assets/js/pages/login.js
@@ -1,3 +1,27 @@
+function rules() {
+  return {
+    email: {
+      required: true,
+      minlength: 3,
+      regex: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+    },
+    password: { required: true, minlength: 8 },
+  };
+}
+function messages() {
+  return {
+    email_required: "Email không được để trống",
+    password_required: "Mật khẩu không được để trống",
+    email_regex: "Email không đúng định dạng",
+  };
+}
+function validateSuccess() {
+  console.log(" validate sucsses");
+}
 let loginInstanceValidate = new Validate({
-  container: "#page_login",
+  container: ".login_form",
+  btnclassSubmit: "btn-login",
+  rules: rules(),
+  messages: messages(),
+  success: validateSuccess,
 });
