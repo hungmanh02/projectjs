954ca73dbb17c922209db6cbcb1684c6d0e71927
hungmanh02 <domanh462@gmail.com>
2023-11-09 11:04:14 +0700

    create category sieu chi tiet

--- assets/js/pages/admin-category.js
+++ assets/js/pages/admin-category.js
@@ -1,3 +1,19 @@
+const formCategory = document.querySelector("#category_form_add");
+// console.log(formCategory);
+function validateSuccess() {
+  // 1. Lấy ra thông tin của danh mục
+  const nameCategory = formCategory.querySelector(".category_name").value;
+  // 2. Tạo ra object chứa thông tin danh mục
+  const newCate = {
+    id: crypto.randomUUID(),
+    name: nameCategory,
+  };
+  // 3. Đưa object vào trong mảng category
+  let categories = JSON.parse(localStorage.getItem("categories")) || [];
+  categories.push(newCate);
+  // 4. Lưu vào trong local
+  localStorage.setItem("categories", JSON.stringify(categories));
+}
 let validateCategory = new Validate({
   container: "#category_form_add",
   btnClassSubmit: "btn_category_save",
@@ -9,7 +25,5 @@ let validateCategory = new Validate({
   messages: {
     category_name_required: "Danh mục không được để trống",
   },
-  success: function () {
-    console.log("success");
-  },
+  success: validateSuccess,
 });
