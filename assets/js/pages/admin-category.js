25d1c7ab582f54db8da4f1d0e859e37091cb12dc
hungmanh02 <domanh462@gmail.com>
2023-11-13 15:21:27 +0700

    show data category

--- assets/js/pages/admin-category.js
+++ assets/js/pages/admin-category.js
@@ -1,5 +1,33 @@
 const formCategory = document.querySelector("#category_form_add");
-// console.log(formCategory);
+function deleteData() {
+  console.log("delete Data");
+}
+function showDataCateFromLocal() {
+  // 1. Lấy toàn bộ danh mục trong localStorage
+  const categories = JSON.parse(localStorage.getItem("categories"));
+  // 2. Xây dựng cấu trúc html cho danh mục
+  let htmlResult = "";
+  categories.forEach(function (element) {
+    htmlResult =
+      htmlResult +
+      `<tr>
+      <td>${element.name}</td>
+      <td>
+        <button class="btn_common btn_edit">Edit</button>
+        <button class="btn_common btn_delete">
+          Delete
+        </button>
+      </td>
+      </tr>`;
+  });
+  // 3. Đưa kết quả toàn bộ danh mục vào tbody của table
+  document.querySelector(".category_table").innerHTML = htmlResult;
+  // Đưa sự kiện delete
+  // Thêm sự kiện delete dữ liệu
+  document.querySelectorAll(".btn_delete").forEach(function (element) {
+    element.addEventListener("click", deleteData);
+  });
+}
 function validateSuccess() {
   // 1. Lấy ra thông tin của danh mục
   const nameCategory = formCategory.querySelector(".category_name").value;
@@ -10,10 +38,14 @@ function validateSuccess() {
   };
   // 3. Đưa object vào trong mảng category
   const categories = JSON.parse(localStorage.getItem("categories")) || [];
-  const categoriesUpdate = [...categories, newCate];
+  const categoriesUpdate = [newCate, ...categories];
   // 4. Lưu vào trong local
   localStorage.setItem("categories", JSON.stringify(categoriesUpdate));
+  // 5. Hiện thị dữ liệu ngay lập tức khi thêm thành công
+  showDataCateFromLocal();
 }
+// Hiện thị dữ liệu category từ local
+showDataCateFromLocal();
 let validateCategory = new Validate({
   container: "#category_form_add",
   btnClassSubmit: "btn_category_save",
