da475563549e24f0c8f9d839ae3ca73e2fe51ace
hungmanh02 <domanh462@gmail.com>
2023-11-14 09:46:29 +0700

    create product chi tiêt

--- assets/js/pages/admin-product.js
+++ assets/js/pages/admin-product.js
@@ -1,4 +1,5 @@
 const selectCate = document.querySelector(".category_wrapper_form");
+const formProduct = document.querySelector("#form_save_product");
 function showCategoryInProduct() {
   // 1. Lấy toàn bộ danh mục trong localStorage
   const cateAll = JSON.parse(localStorage.getItem("categories")) || [];
@@ -14,7 +15,27 @@ function showCategoryInProduct() {
 // Hiện thị danh mục khi load trang lần đầu
 showCategoryInProduct();
 function validateProductSuccess() {
-  console.log("validate success");
+  // 1. Lấy ra value của input và tạo ra object chứa thông tin sản phẩm
+  let objectValue = {};
+  const inputAll = formProduct.querySelectorAll(".form-control-item");
+  inputAll.forEach(function (element) {
+    if (element.name === "category_wrapper_form") {
+      objectValue["category_id"] = element.value;
+    } else {
+      objectValue[element.name] = element.value;
+    }
+  });
+  objectValue.id = crypto.randomUUID();
+  const productType = document.querySelector(".type_product:checked").value;
+  objectValue.product_type = productType;
+  console.log(objectValue);
+  // 2. đưa object vào trong mảng
+  let products = JSON.parse(localStorage.getItem("products")) || [];
+  const productsNew = [objectValue, ...products];
+
+  // 3. Lưu dữ liệu vào local storage
+
+  localStorage.setItem("products", JSON.stringify(productsNew));
 }
 let validateProduct = new Validate({
   container: "#form_save_product",
